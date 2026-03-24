1ccd77ab04648435ded9fd874e95f6960bcbcd7b
AyushDeuja <ayushdeuja11@gmail.com>
2026-03-24 18:55:12 +0545

    added posts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,26 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Posts } from './posts/posts';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Posts],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
-  protected readonly title = signal('first-app');
+  posts = [
+    {
+      id: 1,
+      user: 'John Doe',
+      content: 'Just had an amazing lunch with my family!',
+      comments: ['Sounds Fun', "I'm so Happy"],
+    },
+    {
+      id: 2,
+      user: 'Jane Doe',
+      content: 'I am so excited to be here!',
+      comments: ['I am excited too!', 'I am so excited'],
+    },
+  ];
 }
