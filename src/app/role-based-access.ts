dd20176ca3d248295894243b1240da95dd34bef9
AyushDeuja <ayushdeuja11@gmail.com>
2026-03-25 18:32:37 +0545

    created custom decorator

---
+++ src/app/role-based-access.ts
@@ -0,0 +1,20 @@
+import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+
+@Directive({
+  selector: '[appRoleBasedAccess]',
+})
+export class RoleBasedAccess {
+  constructor(
+    private templateRef: TemplateRef<any>,
+    private viewContainer: ViewContainerRef,
+  ) {}
+
+  @Input() set appRoleBasedAccess(requiredRole: string) {
+    this.viewContainer.clear();
+    const currentUserRole = 'Admin';
+
+    if (currentUserRole === requiredRole) {
+      this.viewContainer.createEmbeddedView(this.templateRef);
+    }
+  }
+}
